96e3f280906e6fea8e6f1b56de277b85a0045391
Sasha Rush <srush@seas.harvard.edu>
2016-01-26 08:24:28 -0500

    HW1

---
+++ HW1.lua
@@ -0,0 +1,29 @@
+-- Only requirement allowed
+require("hdf5")
+
+cmd = torch.CmdLine()
+
+-- Cmd Args
+cmd:option('-datafile', '', 'data file')
+cmd:option('-classifier', 'nb', 'classifier to use')
+
+-- Hyperparameters
+-- ...
+
+
+function main() 
+   -- Parse input params
+   opt = cmd:parse(arg)
+   local f = hdf5.open(opt.datafile, 'r')
+   nclasses = f:read('nclasses'):all():long()[1]
+   nfeatures = f:read('nfeatures'):all():long()[1]
+
+   local W = torch.DoubleTensor(nclasses, nfeatures)
+   local b = torch.DoubleTensor(nclasses)
+
+   -- Train.
+
+   -- Test.
+end
+
+main()
